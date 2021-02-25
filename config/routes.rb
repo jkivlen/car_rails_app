1ed391b829b2155a8fe5028c261e066e2e7e73c5
jkivlen <jimmykivlen@gmail.com>
2021-02-25 12:36:09 -0600

    added routes for cars and makes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,24 @@
 Rails.application.routes.draw do
   
-  get 'makes/new'
-  get 'makes/create'
-  get 'makes/index'
-  get 'makes/show'
-  get 'cars/new'
-  get 'cars/create'
-  get 'cars/index'
-  get 'cars/show'
-  get 'cars/edit'
-  get 'cars/update'
-  get 'cars/destroy'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+
+
+  get '/cars/expensive', to: 'cars#expensive', as: 'expensive_car'
+
+  resources :cars
+
+  resources :makes, only: [:new, :create, :index, :show] do
+    resources :cars, only: [:new, :create, :index]
+  end
 end
